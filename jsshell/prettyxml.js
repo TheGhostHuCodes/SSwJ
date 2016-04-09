c2edc1cdfee9b5d8fe1d3298bfce0fc626c483db
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-04-09 12:06:23 -0700

    Use cmdline argument as filename input

--- jsshell/prettyxml.js
+++ jsshell/prettyxml.js
@@ -1,20 +1,13 @@
 var fs = require("fs");
-var path = require("path");
-var expandTilde = require("expand-tilde");
 var prettyData = require("pretty-data").pd;
 
-var xmlFile =
-  expandTilde("~/Repos/SSwJ/MyFoodapediaData/Food_Display_Table.xml");
-
-console.log(xmlFile);
-
+var xmlFile = process.argv[2];
 if (!fs.existsSync(xmlFile)) {
     console.warn(`ERROR file doesn't exist: ${xmlFile}`);
     process.exit(1);
 }
 
 var xml = fs.readFileSync(xmlFile, 'utf8');
-console.log(xml);
 
 var prettyXml = prettyData.xml(xml);
 console.log(prettyXml);
