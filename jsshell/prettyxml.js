ec7059aaae10d1cf87bfb703ade8d8e17f3b8873
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-04-09 15:50:03 -0700

    Refactor prettyxml.js with shelljs/global methods

--- jsshell/prettyxml.js
+++ jsshell/prettyxml.js
@@ -1,13 +1,14 @@
 var fs = require("fs");
+require("shelljs/global");
 var prettyData = require("pretty-data").pd;
 
 var xmlFile = process.argv[2];
 if (!fs.existsSync(xmlFile)) {
     console.warn(`ERROR file doesn't exist: ${xmlFile}`);
-    process.exit(1);
+    exit(1);
 }
 
-var xml = fs.readFileSync(xmlFile, 'utf8');
+var xml = cat(xmlFile);
 
 var prettyXml = prettyData.xml(xml);
 console.log(prettyXml);
