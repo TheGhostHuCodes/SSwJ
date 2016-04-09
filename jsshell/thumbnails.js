cd1483270865d5a595c8aff7d5744ad9479cbd11
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-04-09 12:44:16 -0700

    Use lwip library to open and read image metadata

--- jsshell/thumbnails.js
+++ jsshell/thumbnails.js
@@ -1,4 +1,6 @@
 require("shelljs/global");
+var lwip = require("lwip");
+var path = require("path");
 
 var images = ls("../images/*.jpg"); // fs.readdirSync
 echo(images);                       // console.log
@@ -6,3 +8,15 @@ echo(images);                       // console.log
 var thumbsDirectory = "../images/thumbs";
 rm("-rf", thumbsDirectory); // fs.rmdirSync
 mkdir(thumbsDirectory);     // fs.mkdirSync
+
+images.forEach(imageFile => {
+    lwip.open(imageFile, (error, image) => {
+        echo(imageFile);
+        echo(`Width: ${image.width()}, Height: ${image.height()}`);
+
+        var fileName = path.basename(imageFile);
+        echo(`basename: ${fileName}`);
+        var thumbFile = path.join(thumbsDirectory, fileName);
+        echo(`thumbFile: ${thumbFile}`);
+    });
+});
