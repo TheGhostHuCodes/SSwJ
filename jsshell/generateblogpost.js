6d589f8c9e3c54c877152580804b875bd66f26a6
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-04-08 22:14:15 -0700

    Write blog header out to file

--- jsshell/generateblogpost.js
+++ jsshell/generateblogpost.js
@@ -1,9 +1,19 @@
 var moment = require("moment");
+var fs = require("fs");
 
-var date = moment().format("YYYY-MM-DD-HH-mm")
+var date = moment().format("YYYY-MM-DD-HH-mm");
 var userName = process.env.USER;
 
-console.log(`--Date: ${date}`);
-console.log(`--User: ${userName}`);
-console.log(`--Title: `);
-console.log(`--------------------------------------------------------------------------------`);
+var header = `
+--Date: ${date}
+--User: ${userName}
+--Title:
+--------------------------------------------------------------------------------`;
+
+var fileName = `${ date }.md`;
+
+if (fs.existsSync(fileName)) {
+    console.log(`ERROR file exists : ${fileName}`);
+} else {
+    fs.writeFileSync(fileName, header);
+}
