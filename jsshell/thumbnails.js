3749a78b2702b90cc29a27e9a4c3b3a6fed2abce
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2016-04-09 21:59:33 -0700

    Set default size to 200

--- jsshell/thumbnails.js
+++ jsshell/thumbnails.js
@@ -7,7 +7,7 @@ var program = require('commander');
 
 program.version('1.0')
   .option('-s, --size <integer>',
-          'The image size to shrink to, both height and width.')
+          'The image size to shrink to, both height and width.', 200)
   .option('-d, --directory <path>',
           'The directory of images to create thumbnails for.')
   .parse(process.argv);
